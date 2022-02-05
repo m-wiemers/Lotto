cefc47e55b1b0b8d7b9d0488bcd9f126b5b3a183
Marcel Wiemers <77615352+m-wiemers@users.noreply.github.com>
2022-02-05 16:53:48 +0100

    try to resolve

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "./styles.css";
 import styled from "styled-components";
-import Table from "./components/Table";
-import { SelectedNumberFieldWrapper, Error } from "./components/Fields";
-import NumberField from "./components/NumberField";
-import Button from "./components/Button";
+import Table from "../components/Table.tsx";
+import { SelectedNumberFieldWrapper, Error } from "../components/Fields.tsx";
+import NumberField from "../components/NumberField.tsx";
+import Button from "../components/Button.tsx";
 
 const getWinNumbers = () => {
   const arr = [];
